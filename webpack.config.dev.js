d516bd9e93c1d31e07b0aa73c3f528214b16a0b6
ruanhc <twboss8@126.com>
2018-10-25 16:50:52 +0800

    编译输出路径设置

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,12 +8,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin'); //引入插件
 module.exports = {
   entry: './src/app.js',  //入口文件
   output: {               //输出配置，打包后的文件
-    path: path.resolve(__dirname,'dist',),  //输出到哪里，只能用绝对路径，这里使用node path模块，放到项目根目录下dist文件夹
-    filename: 'main.js' //文件名
+    path: path.resolve(__dirname,'dist/'),  //输出到哪里，只能用绝对路径，这里使用node path模块，放到项目根目录下dist文件夹
+    filename: 'assets/js/main.js', //文件名
+    publicPath: './',  //所有资源的基础路径，而且一定是/结尾
   },
   plugins: [  //插件
     new HtmlWebpackPlugin({ //利用这个插件打包自动生成index文件
-      filename: 'index.html', //打包后的文件名
+      filename: 'index.html', //打包后的文件名 路径 这里因为上面配置了统一打包到assets文件夹，因此index文件跳出一层
       template: 'src/index.html'  //文件模板
     })
   ],
@@ -61,6 +62,8 @@ module.exports = {
         loader: 'url-loader', //url-loader默认都转换为base64编码形式，可以使用limit约束
         options: {
           limit: 10000, //以b为单位，1024b=1kb，超过这个值就会交给file-loader打包处理，小于则使用base64转码
+          name: 'assets/img/[name]_[hash:6].[ext]',  // 设置编译后的文件名 路径
+
         }
       },
       //字体也是用url-loader/file-loader处理
@@ -73,5 +76,6 @@ module.exports = {
   devServer: {
     port: 9966, //服务端口
     open: false, //是否自动打开到浏览器
+    publicPath: '/',  // 服务器打包资源后的输出路径
   }
 }
